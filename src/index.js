0badf163e05d595ca56a1133778e3b290d28a653
Ankit Singh <ankit.singh@digimantra.com>
2024-05-31 12:03:34 +0530

    apierror and api response cretaion

--- src/index.js
+++ src/index.js
@@ -19,7 +19,15 @@ dotenv.config({
   path: './env'
 });
 
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at : ${process.env.PORT}`);
+  })
+})
+.catch((err) =>{
+  console.log("MONGO db connectetion failed !!!" , err);
+})
 
 // import express from "express";
 
